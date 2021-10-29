e8ad35573c16cf0c7d99039c47aaa62dd9c4c689
Minji <321963741@naver.com>
2021-10-29 10:37:16 +0900

    counter html,css,js done

---
+++ 02-counter/starter/index.js
@@ -0,0 +1,24 @@
+const btns = document.querySelectorAll('.btn-list');
+const number = document.querySelector('.number');
+
+const handleClick = (e) => {
+  const targetNumber = number.textContent;
+  console.log(targetNumber);
+  const type = e.target.dataset.type;
+  if (type === 'decrease') {
+    const decreaseNumber = targetNumber - 1;
+    if (decreaseNumber < 0) {
+      return 0;
+    }
+    number.textContent = decreaseNumber;
+  }
+  if (type === 'increase') {
+    const increaseNumber = Number(targetNumber) + 1;
+    number.textContent = increaseNumber;
+  }
+  if (type === 'reset') {
+    number.textContent = 0;
+  }
+};
+
+btns.forEach((btn) => btn.addEventListener('click', handleClick));
